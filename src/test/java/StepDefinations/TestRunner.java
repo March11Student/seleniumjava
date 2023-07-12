02afb69a395bb51f69268843ac25d038e7865e0f
March11Student <iqratechstudent@gmail.com>
2023-07-12 11:38:23 +0530

    remove testhelo

--- src/test/java/StepDefinations/TestRunner.java
+++ src/test/java/StepDefinations/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:\\workspace\\seleniumjava\\src\\test\\resources\\Features\\iqrashopingDemo.feature"
+        features = "/Users/vishalu/Downloads/Class2023/seleniumjava/src/test/resources/Features"
         ,glue={"StepDefinations"}
 )
 
